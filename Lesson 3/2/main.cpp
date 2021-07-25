4b5f0a65139a6ba4898744cd54b2c6a28f65acc8
Dima Radin <79787976+Dimonn228@users.noreply.github.com>
2021-07-25 18:31:25 +0300

    Add files via upload

--- Lesson 3/2/main.cpp
+++ Lesson 3/2/main.cpp
@@ -9,20 +9,32 @@ int main()
 		static int array[10] = { 0 };
 		int card = 0, number = 0, amount = 0;
 
-		cout << "Na kakuyu kartu polizhit? ";
+		cout << "Na kakuyu kartu polozhit? ";
 		cin >> card;
+
 		cout << "Skolko? ";
 		cin >> number;
 
+		if (number < 0){
+
+            cout << "Nepravilnaya summa!\n\n";
+            continue;
+		}
+
 		array[card - 1] += number;
 
-		for (int i = 0; i < 10; i++)
-			cout << array[i] << " ";
+		for (int i = 0; i < 10; i++){
 
-		for (int i = 0; i < 10; i++)
+			cout << array[i] << " ";
+		}
+
+		for (int i = 0; i < 10; i++){
+
 			amount += array[i];
+		}
 
 		cout << "\nSumma: " << amount << "\n\n";
 	}
 
+    return 0;
 }
